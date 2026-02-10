eeff094d200ae26dc0a86a6034258c5da6353a30
COMRADE040 <rafiulislam040@gmail.com>
2026-02-10 23:15:03 +0600

    comment

--- index.js
+++ index.js
@@ -116,10 +116,7 @@ async function run() {
 
     // Delete __ Delete
     app.delete('/products/:id', async (req, res) => {
-        // const id = req.params.id;
-        // const query = {_id: new ObjectId(id)}
-        // const result = await productCollection.deleteOne(query)
-        // res.send(result)
+        
 
 
 
